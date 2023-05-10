09cc7282f7c6d79c53f6b409ad9b92e10ea115c0
anderson-bassan <bassan.programador@gmail.com>
2023-05-10 11:11:19 -0300

    update port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 80;
 
 app.set('view engine', 'hbs');
 
@@ -11,4 +12,4 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.listen(80);
+app.listen(port);
